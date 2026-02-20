805db4d91c01a2d4c48f5a6735f4e0f3164e0b74
Ivan Farel <ivanfarel07@gmail.com>
2026-02-20 15:24:02 +0700

    Add files via upload

---
+++ P4/Praktikum4-LnkedList.py
@@ -0,0 +1,35 @@
+#================================================================================
+#Nama   : 
+#NIM    :
+#Kelas  :
+#================================================================================
+
+#================================================================================
+#Implementasi Dasar : Noode pada Linked List
+#================================================================================
+
+class Node :
+    #Konstrutor yang dijalankan secara otomatis ketika class node dipanggil / diinstalasi
+    def __init__(self, data):
+        self.data = data #menyimpan nilai aatu data pada list
+        self.next = None #pointer ini menunjuk ke note berikutnya (awal=none)
+
+#1) membuat node dengan instantiasi class node
+nodeA =  Node("A")
+nodeB =  Node("B")
+nodeC =  Node("C")
+
+#2) Menghubungkan Node : A -> B -> C -> None
+head = nodeA
+nodeA.next = nodeB
+nodeB.next = nodeC
+
+#3) Traversal : MENELUSURI NODE dari head sampai ke None
+current = head
+while current is not None :
+    print(current.data) #menampilan data node saat ini
+    current = current.next #pindah ke node selanjutnya
+
+
+
+    
